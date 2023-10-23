c2c29bf35c1b43e5dae86729466ad96e8f52b694
32Valik92 <valokstrukalo@gmail.com>
2023-10-23 11:34:03 +0300

    Config reducer and store

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,28 @@
+import {CssBaseline, ThemeProvider} from "@mui/material";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import {Provider} from "react-redux";
+import {BrowserRouter} from "react-router-dom";
 
 import App from "./App";
+import {setupStore} from "./redux";
+import {theme} from "./theme";
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
 );
+
+const store = setupStore();
+
 root.render(
-   <App/>
+   <>
+      <CssBaseline/>
+      <ThemeProvider theme={theme}>
+         <BrowserRouter>
+            <Provider store={store}>
+               <App/>
+            </Provider>
+         </BrowserRouter>
+      </ThemeProvider>
+   </>
 );
---
+++ src/redux/store.ts
@@ -0,0 +1,27 @@
+import {combineReducers, configureStore} from "@reduxjs/toolkit";
+
+// Container with our reducers
+const rootReducer = combineReducers({
+    
+});
+
+// Our main store
+const setupStore = () => configureStore({
+   reducer: rootReducer
+});
+
+// Our function-returning types
+type RootState = ReturnType<typeof rootReducer>;
+type AppStore = ReturnType<typeof setupStore>;
+type AppDispatch = AppStore["dispatch"];
+
+export type {
+   RootState,
+   AppStore,
+   AppDispatch
+};
+
+export {
+   setupStore
+};
+
